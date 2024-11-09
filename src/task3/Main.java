f15e2c21e58ec2e3774000eda80ff25b30360497
Daniar <dakimaliev@gmail.com>
2024-11-09 11:36:32 +0600

    first commit

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -1,11 +1,18 @@
 package task3;
 
+/**Напишите обобщенный класс Pair, который представляет собой
+пару значений разного типа. Класс должен иметь методы
+getFirst(), getSecond() для получения значений каждого
+из составляющих пары, а также переопределение метода
+toString(), возвращающее строковое представление пары.
+Работу сдать в виде ссылки на гит репозиторий.
+ */
 public class Main {
     public static void main(String[] args) {
 
         Pair b = new Pair(1,"Gulpreet Kaur");
         //System.out.println(b);
-        System.out.println(b.toString());
+        System.out.println(b);
 
     }
 }
